a3805148df62ec1d80bf4e6e0eb0dd6314e3f1f8
sandy <sandytang2406@gmail.com>
2019-03-14 21:43:46 +0100

    my first commit

--- src/main/java/at/htl/klassestudent/Klasse.java
+++ src/main/java/at/htl/klassestudent/Klasse.java
@@ -1,4 +1,40 @@
 package at.htl.klassestudent;
 
 public class Klasse {
+    int klassenbezeichnung;
+    Student[] students;
+
+
+
+    //Sortiert immer 2 nebeneinanderanliegende Elemte
+    public static void sortBubble(Student students[]){
+
+
+        int length = students.length;
+        for (int i = 0; i < length-1; i++)
+        for (int j = 0; j < length-i-1; j++)
+            if (students[j].getNachname().charAt(0) > students[j+1].getNachname().charAt(0))
+            {
+                // swap arr[j+1] and arr[i]
+                Student temp = students[j];
+                students[j] = students[j+1];
+                students[j+1] = temp;
+            }
+    }
+
+
+    //Sortiert nach input
+    public static void sortInsert(Student students[]){
+        int length = students.length;
+        for (int i = 1; i < length; ++i) {
+            Student help = students[i];
+            int j = i - 1;
+
+            while (j >= 0 && students[j].getNachname().charAt(0) > help.getNachname().charAt(0)) {
+                students[j + 1] = students[j];
+                j = j - 1;
+            }
+            students[j + 1] = help;
+        }
+    }
 }
--- src/test/java/at/htl/klassestudent/KlasseStudentTest.java
+++ src/test/java/at/htl/klassestudent/KlasseStudentTest.java
@@ -1,7 +1,67 @@
 package at.htl.klassestudent;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 public class KlasseStudentTest {
+    @BeforeAll
+    static void setup() {
 
+    }
+
+    @Test
+    void unsortiert01() {
+        String[] name = {"Marie Mueller", "Joseph Acker", "Sebastian Berger", "Martina Bauer"};
+
+        Klasse.sortBubble(name);
+        Arrays.sort(expected);
+
+        assertThat(name,is(expected));
+    }
+
+    @Test
+    void sortiert() {
+        String[] name = {"Joseph Acker", "Martina Bauer", "Sebastian Berger", "Marie Mueller"};
+
+        Klasse.sortBubble(name);
+        Arrays.sort(expected);
+
+        assertThat(name,is(expected));
+    }
+
+    @Test
+    void unsortiert02() {
+        String[] name = {"Marie Mueller", "Joseph Acker", "Martina Bauer", " Sebastian Berger"};
+
+        Klasse.sortBubble(name);
+        Arrays.sort(expected);
+
+        assertThat(name,is(expected));
+    }
+
+    @Test
+    void unsortiert03() {
+        String[] name = {" Sebastian Berger", "Joseph Acker", "Martina Bauer", " Marie Mueller"};
+
+        Klasse.sortBubble(name);
+        Arrays.sort(expected);
+
+        assertThat(name,is(expected));
+    }
+
+    @Test
+    void unsortiert04() {
+        String[] name = {" Martina Bauer", "Marie Mueller", "Sebastian Berger", " Joseph Acker"};
+
+        Klasse.sortBubble(name);
+        Arrays.sort(expected);
+
+        assertThat(name,is(expected));
+    }
 }
